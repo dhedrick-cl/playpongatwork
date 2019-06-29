2eb8f3d6fbc3f99fb43977dc720af46aa1ae32fa
dhedrick-cl <dhedrick@corelogic.com>
2019-06-29 12:53:44 -0400

    edit match settings

--- src/app/components/match/match.component.ts
+++ src/app/components/match/match.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatchService } from '../../services/match.service';
+import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-match',
@@ -7,7 +8,8 @@ import { MatchService } from '../../services/match.service';
   styleUrls: ['./match.component.scss']
 })
 export class MatchComponent implements OnInit {
-
+  editMatch: boolean = false;
+  private editMatchForm;
   constructor(public matchService: MatchService) {
     this.matchService.currentMatch.challenger = 'Travis';
     this.matchService.currentMatch.opponent = 'Daniel';
@@ -15,7 +17,22 @@ export class MatchComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+    this.editMatchForm = new FormGroup({
+      challengerName: new FormControl("", Validators.required),
+      challengerScore: new FormControl("", Validators.required),
+      opponentName: new FormControl("", Validators.required),
+      opponentScore: new FormControl("", Validators.required),
+    });
+  }
+  onSubmit(form: NgForm) {
+    let challengerName = this.editMatchForm.get("challengerName").value;
+    let challengerScore = this.editMatchForm.get("challengerScore").value;
+    let opponentName = this.editMatchForm.get("opponentName").value;
+    let opponentScore = this.editMatchForm.get("opponentScore").value;
+    console.log('challenger name: ', challengerName, ' challenger score: ', challengerScore, 'opponent name: ', opponentName, ' oppponent score: ', opponentScore)
+    // this.authService.login(email, password);
+    //this.router.navigate(["search"]);
+
   }
 
 }
